6fd43d447ca575c869a688540b3c9ea9f613c505
tesluk <atesluk@rambler.u>
2012-10-11 12:51:27 +0300

    Отключил django evolution

--- Basatashop/settings.py
+++ Basatashop/settings.py
@@ -139,7 +139,7 @@ INSTALLED_APPS = (
     'Basatashop.Basket',
     'Basatashop.instructions',
     'Basatashop.news',
-    'django_evolution',
+    #'django_evolution',
 )
 
 # A sample logging configuration. The only tangible logging
