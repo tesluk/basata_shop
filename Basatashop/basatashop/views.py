6b31e9e7aff81bab073903d41df0b144642e6f71
Sergiy Skyrda <atemi10s@meta.ua>
2012-11-24 20:30:26 +0200

    Check if user in session for each view

--- Basatashop/basatashop/views.py
+++ Basatashop/basatashop/views.py
@@ -10,19 +10,32 @@ from django.template.context import RequestContext
 def index (request):
     c = {}
     c.update(csrf(request))
-    return render_to_response('index.html', {},context_instance=RequestContext(request))
+    if "user" in request.session:    
+        return render_to_response('index.html', {'user':request.session['user']},context_instance=RequestContext(request))
+    else:
+        return render_to_response('index.html', {},context_instance=RequestContext(request))
+        
 
-def about (request):
+def about (request):    
     c = {}
     c.update(csrf(request))
-    return render_to_response('about.html', {},context_instance=RequestContext(request))
+    if "user" in request.session:
+        return render_to_response('about.html', {'user':request.session['user']},context_instance=RequestContext(request))
+    else:
+        return render_to_response('about.html', {},context_instance=RequestContext(request))
 
 def contacts (request):
     c = {}
     c.update(csrf(request))
-    return render_to_response('contacts.html', {},context_instance=RequestContext(request))
+    if "user" in request.session:
+        return render_to_response('contacts.html', {'user':request.session['user']},context_instance=RequestContext(request))
+    else:
+        return render_to_response('contacts.html', {},context_instance=RequestContext(request))
 
 def delivery (request):
     c = {}
     c.update(csrf(request))
-    return render_to_response('delivery_terms.html', {},context_instance=RequestContext(request))
+    if "user" in request.session:
+        return render_to_response('delivery_terms.html', {'user':request.session['user']},context_instance=RequestContext(request))
+    else:
+        return render_to_response('delivery_terms.html', {},context_instance=RequestContext(request))
