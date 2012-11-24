fede2f8f033c8713f1eeba18311e871b9bca65fc
tesluk <atesluk@rambler.ru>
2012-11-24 17:38:22 +0200

    Update Basatashop/Entities/models.py

--- Basatashop/Entities/models.py
+++ Basatashop/Entities/models.py
@@ -35,30 +35,85 @@ class Instruction (models.Model):
 class Product (models.Model):
     prod_type = models.ForeignKey(Product_type)
     name = models.CharField(max_length=60)
-    
-    price = models.FloatField()
-    quantity = models.IntegerField()
-    model3D = models.FileField(upload_to='Entities/static/products', blank=True, null=True)
-        
+    producer = models.CharField(max_length=30, blank=True, null=True)
     sdescription = models.TextField(blank=True, null=True)
     picture = models.ImageField(upload_to='Entities/static/products', blank=True, null=True)
     picture_l = models.ImageField(upload_to='Entities/static/products', blank=True, null=True)
     picture_b = models.ImageField(upload_to='Entities/static/products', blank=True, null=True)
-    
-    
-    description = models.TextField()
-    instrucrion = models.ForeignKey(Instruction, blank=True, null=True)
+    model = models.ImageField(upload_to='Entities/static/products', blank=True, null=True) #ìîæëèâî, òèï ïîëÿ äîâåäåòüñÿ çì³íèòè
+    quantity = models.IntegerField()
+    #instrucrion = models.ForeignKey(Instruction, blank=True, null=True)
     
     def __unicode__(self):
         return self.name
     
 class Characteristic (models.Model):
     product = models.ForeignKey(Product)
-    charac_type = models.CharField(max_length=30, blank=True, null=True)
-    name = models.CharField(max_length=30, blank=True, null=True)
-        
+    description = models.TextField()
+    #charac_type = models.CharField(max_length=30, blank=True, null=True)
+    #name = models.CharField(max_length=30, blank=True, null=True)
+    #size = models.IntegerField()
+    #prise = models.FloatField()
+    
     def __unicode__(self):
-        return self.name
+        return self.description
+    
+class Price (models.Model):
+    product = models.ForeignKey(Product)
+    value = models.FloatField()
+    date_init = models.DateTimeField()  
+    
+    def __unicode__(self):
+        return self.value  
+    
+class Delivery (models.Model):
+    supplier = models.CharField(max_length=30, blank=True, null=True)
+    date = models.DateTimeField()
+    total = models.IntegerField()
+    
+class Del_item (models.Model):
+    delivery = models.ForeignKey(Delivery)
+    product = models.ForeignKey(Product)
+    quantity = models.IntegerField()
+    price = models.FloatField()    
+
+class Basket (models.Model):
+    BASKET_CHOICES = (
+                      (u'N', u'New'),
+                      (u'R', u'Ready'),
+                      (u'F', u'Finished'),
+                      )
+    user = models.ForeignKey(User) 
+    courier = models.ForeignKey(User)
+    adding_time = models.DateTimeField()
+    address = models.CharField(max_length=100)
+    tel = models.CharField(max_length=20)
+    comment = models.TextField(blank=True, null=True)
+    #summ = models.FloatField()
+    total = models.FloatField()
+    btype = models.CharField(max_length=2, choices=BASKET_CHOICES) 
+    
+class Order (models.Model):
+    basket = models.ForeignKey(Basket)
+    product = models.ForeignKey(Product)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+    #characteristic = models.ForeignKey(Characteristic)
+    #numb = models.IntegerField()
+    
+#class Company (models.Model):
+#    user = models.ForeignKey(User)
+#    name = models.CharField(max_length=50)
+#    tel = models.CharField(max_length=20)
+#    city = models.CharField(max_length=50)
+#    address = models.CharField(max_length=100)
+#    post = models.CharField(max_length=50)
+#    description = models.TextField()
+    
+#class DocumentFile (models.Model):
+#    doc_file = models.FileField(upload_to = 'Entities/static/files')
+#    doc_type = models.CharField(max_length = 10)
+#    description = models.TextField()
     
     
 class News (models.Model):
@@ -73,39 +128,4 @@ class Question (models.Model):
     mail = models.EmailField()
     text = models.TextField()
     adding_time = models.DateTimeField()
-    answer = models.TextField(blank=True, null=True)
-    
-
-class Basket (models.Model):
-    BASKET_CHOICES = (
-                      (u'N', u'New'),
-                      (u'R', u'Ready'),
-                      (u'F', u'Finished'),
-                      )
-    user = models.ForeignKey(User)
-    adding_time = models.DateTimeField()
-    address = models.CharField(max_length=100)
-    tel = models.CharField(max_length=20)
-    comment = models.TextField(blank=True, null=True)
-    summ = models.FloatField()
-    btype = models.CharField(max_length=2, choices=BASKET_CHOICES) 
-    
-class Order (models.Model):
-    basket = models.ForeignKey(Basket)
-    characteristic = models.ForeignKey(Characteristic)
-    numb = models.IntegerField()
-    
-class Company (models.Model):
-    user = models.ForeignKey(User)
-    name = models.CharField(max_length=50)
-    tel = models.CharField(max_length=20)
-    city = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
-    post = models.CharField(max_length=50)
-    description = models.TextField()
-    
-class DocumentFile (models.Model):
-    doc_file = models.FileField(upload_to = 'Entities/static/files')
-    doc_type = models.CharField(max_length = 10)
-    description = models.TextField()
-    
+    answer = models.TextField(blank=True, null=True)    
